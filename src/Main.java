43336cb8cc619730bc50c380ebfe1c683417b79e
esmanurgokkaya <114703355+esmanurgokkaya@users.noreply.github.com>
2023-12-10 13:59:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
             // yeni bir pencere oluşturulur
